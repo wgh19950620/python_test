7592bfd107c0d4801e9a3c38039a68229d74045f
wgh19950620 <whui_wang@163.com>
2019-05-10 14:20:10 +0800

    test

--- com.src.wgh/annotation/annotation.py
+++ com.src.wgh/annotation/annotation.py
@@ -4,3 +4,11 @@ def a():
 
 
 print(a.__doc__)
+
+b = 1
+while b < 7:
+    if b % 2 == 0:
+        print(b, "is even")
+    else:
+        print(b, "is odd")
+    b += 1
--- com.src.wgh/conditionExpression/dog_age.py
+++ com.src.wgh/conditionExpression/dog_age.py
@@ -4,7 +4,7 @@ if age < 0:
     print("狗狗年龄怎么能负数呢！")
 elif age == 1:
     print("相当于人14岁")
-elif age ==2:
+elif age == 2:
     print("相当于人22岁")
 elif age > 2:
     person = 22 + (age - 2) * 5
